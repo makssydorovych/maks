d874d98056e77de78b42700d4fd07221fa05ed57
Maksym <maksymsydorovych@gmail.com>
2022-11-03 17:47:56 +0100

    8

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -4,6 +4,7 @@ import s from './App.module.css'
 import HW5 from "../s2-homeworks/hw05/HW5";
 import HW6 from "../s2-homeworks/hw06/HW6";
 import HW7 from "../s2-homeworks/hw07/HW7";
+import HW8 from "../s2-homeworks/hw08/HW8";
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
             <HW5/>
             <HW6/>
             <HW7/>
+            <HW8/>
         </div>
     )
 }
