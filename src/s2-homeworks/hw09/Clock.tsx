6bdc91b152154fc46357c5ad49106a9cd8f1d578
Maksym <maksymsydorovych@gmail.com>
2023-01-17 14:40:55 +0100

    9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -9,14 +9,10 @@ function Clock() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
-    const stop = () => {
-        clearInterval(timerId)
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
 
-    }
     const start = () => {
-        stop()
-        const id: number = + setInterval(()=>{
+        stop();
+        const id: any = setInterval(()=>{
             setDate(new Date())
         }, 1000)
         setTimerId(id)
@@ -25,7 +21,11 @@ function Clock() {
 
     }
 
+    const stop = () => {
+        clearInterval(timerId)
+        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
 
+    }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
         setShow(true);
@@ -34,8 +34,8 @@ function Clock() {
         setShow(false);
     }
 console.log(date)
-    const stringTime = date?.toLocaleTimeString() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date?.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date?.toLocaleTimeString('en-Us') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date?.toLocaleDateString('en-Gb') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date?.toLocaleDateString('en',{weekday: 'long'}) || <br/> // пишут студенты
