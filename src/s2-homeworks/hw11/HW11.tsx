ded5759bbb9b32c69c03eecf33b0f65b25b2f32e
Maksym <maksymsydorovych@gmail.com>
2023-01-24 11:03:15 +0100

    11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, {SetStateAction, useState} from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
@@ -15,11 +15,14 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = ((event: Event, value: any) => {
+    const change = ((event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-
-            setValue1(value)
-            setValue2(100-value)
+            if(Array.isArray(value)) {
+                setValue1(value[0])
+                setValue2(value[1])
+            }else {
+                setValue1(value)
+            }
 
     })
 
